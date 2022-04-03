3034b452f98e0600cb2927ac9226d814ddbeb04b
yarne123456789 <yarne1234567@gmail.com>
2022-04-03 14:56:13 +0200

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,11 @@
+local event = game.ReplicatedStorage.team_switch
+
+event.OnServerEvent:Connect(function(plr, team)
+	if game.Teams:FindFirstChild(team) then
+		local char = plr.Character
+		local humanoid =char:FindFirstChild("Humanoid")
+
+		plr.Team=game.Teams:FindFirstChild(team)
+		plr:LoadCharacter()
+	end
+end)
